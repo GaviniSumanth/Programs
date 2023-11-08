fa4b03e3ba18e970a3c776914a34fe206629fd08
gavinisumanth <sumanth_edu@outlook.com>
2023-11-08 20:33:42 +0530

    remove old programs

--- SPQT/Practice/Data.R
+++
@@ -1,8 +0,0 @@
-# Ungrouped to Grouped
-data <- sample(100, 10, replace = TRUE)
-l <- length(data)
-r <- range(data)
-bins <- seq(r[1], r[2], by = 10)
-class_interval <- cut(data, bins, right = FALSE)
-table <- table(class_interval)
-transform(table)
